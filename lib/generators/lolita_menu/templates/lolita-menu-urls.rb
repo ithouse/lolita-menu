9d7d24b32f573374a9820c2458e04e1574226005
Arturs Meisters <arturs@ithouse.lv>
2011-08-24 13:56:46 +0300

    task bug fix

--- lib/generators/lolita_menu/templates/lolita-menu-urls.rb
+++ lib/generators/lolita_menu/templates/lolita-menu-urls.rb
@@ -3,4 +3,8 @@ Lolita::Menu::Urls.create do
  # add "/contacts", "Contacts"
  # add "/pages/my-contact-page"
  # add root_path
+ # # You can collect data from you models
+ # Pages.where("created_at > ?", 30.days.ago).each do |page|
+ #   add page_path(page), page.title
+ # end
 end
